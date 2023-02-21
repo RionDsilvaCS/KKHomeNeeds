3173c21336a0fa7b09c5da85840d92a9d185e58a
RionDsilva7 <dsilvarion@gmail.com>
2023-02-21 22:55:21 +0530

    "initial "

--- app/src/main/java/com/example/kkhomeneeds/MainActivity.java
+++ app/src/main/java/com/example/kkhomeneeds/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    int a,b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
 
     }
