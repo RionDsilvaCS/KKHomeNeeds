02ca2f4076c73dd9ea77989d616ffff6e8e4a700
RionDsilva7 <dsilvarion@gmail.com>
2023-02-22 22:26:09 +0530

    "initiated splash screen"

---
+++ app/src/main/java/com/example/kkhomeneeds/splashActivity.java
@@ -0,0 +1,42 @@
+package com.example.kkhomeneeds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.os.Handler;
+
+public class splashActivity extends AppCompatActivity {
+
+    Animation bounce;
+    ImageView logo, eclippse1,eclippse2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+
+        logo=findViewById(R.id.logo);
+        eclippse1=findViewById(R.id.downe);
+        eclippse2=findViewById(R.id.upe);
+
+        bounce = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce);
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                logo.startAnimation(bounce);
+                eclippse2.startAnimation(bounce);
+                eclippse1.startAnimation(bounce);
+            }
+        }, 1000);
+
+
+
+
+    }
+}
