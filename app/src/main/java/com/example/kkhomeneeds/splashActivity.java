27645ce7349b74477e6924671070182a2f628fdd
RionDsilva7 <dsilvarion@gmail.com>
2023-02-26 16:08:20 +0530

    "Update on splash screen video"

--- app/src/main/java/com/example/kkhomeneeds/splashActivity.java
+++ app/src/main/java/com/example/kkhomeneeds/splashActivity.java
@@ -5,26 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Looper;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.os.Handler;
+import android.widget.RelativeLayout;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class splashActivity extends AppCompatActivity {
 
     Animation bounce;
-    ImageView logo, eclippse1,eclippse2;
+    ImageView logo;
+    RelativeLayout main;
+    Button signIn, signUp;
+    LottieAnimationView anim1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Intent secondActivityIntent = new Intent(this, MainActivity.class);
         Handler handler = new Handler();
 
-        Intent secondActivityIntent = new Intent(this, MainActivity.class);
-        startActivity(secondActivityIntent);
+        logo = findViewById(R.id.logo);
+        main = findViewById(R.id.welcome_page);
+        signIn = findViewById(R.id.signin);
+        signUp = findViewById(R.id.signup);
+        anim1 = findViewById(R.id.video);
+
+        logo.setVisibility(View.INVISIBLE);
+        main.setVisibility(View.INVISIBLE);
 
 
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                anim1.setVisibility(View.INVISIBLE);
+                logo.setVisibility(View.VISIBLE);
+                main.setVisibility(View.VISIBLE);
 
+            }
+        }, 6000);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(secondActivityIntent);
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(secondActivityIntent);
+            }
+        });
 
     }
 }
