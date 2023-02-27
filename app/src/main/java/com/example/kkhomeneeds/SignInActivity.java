adc05f930d6e20b6e45d725cc4851a0f96b18e9f
puneethx <puneethreddyt2004@gmail.com>
2023-02-27 20:14:35 +0530

    "Sign Up & In Activity Java"

--- app/src/main/java/com/example/kkhomeneeds/SignInActivity.java
+++ app/src/main/java/com/example/kkhomeneeds/SignInActivity.java
@@ -3,8 +3,15 @@ package com.example.kkhomeneeds;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+    EditText userName = findViewById(R.id.editTextTextPersonName2),password = findViewById(R.id.editTextTextPersonName3);
+    TextView forgot_pass;
+    Button SignIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/kkhomeneeds/SignUpActivity.java
+++ app/src/main/java/com/example/kkhomeneeds/SignUpActivity.java
@@ -1,14 +1,21 @@
 package com.example.kkhomeneeds;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText userName = findViewById(R.id.editTextTextPersonName2),email = findViewById(R.id.editTextTextPersonName3),password = findViewById(R.id.editTextTextPersonName4);
+    TextView forgot_pass;
+    Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 }
