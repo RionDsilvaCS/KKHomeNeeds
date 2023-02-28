739da3e408bf5c72ad74efe7fa44e45795c11164
puneethx <puneethreddyt2004@gmail.com>
2023-03-01 00:24:38 +0530

    "Firebase Login"

--- app/src/main/java/com/example/kkhomeneeds/SignInActivity.java
+++ app/src/main/java/com/example/kkhomeneeds/SignInActivity.java
@@ -1,21 +1,101 @@
 package com.example.kkhomeneeds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
-    EditText userName = findViewById(R.id.editTextTextPersonName2),password = findViewById(R.id.editTextTextPersonName3);
-    TextView forgot_pass;
-    Button SignIn;
-
-
+    EditText email,password;
+    Button signIn;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView textClick;
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent (getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.editTextTextPersonName2);
+        password = findViewById(R.id.editTextTextPersonName3);
+        signIn = findViewById(R.id.signin);
+        progressBar = findViewById(R.id.Progress);
+        textClick = findViewById(R.id.registerNow);
+        textClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(view.VISIBLE);
+                email = findViewById(R.id.editTextTextPersonName2);
+                password = findViewById(R.id.editTextTextPersonName3);
+                String emailS,passwordS;
+                emailS = String.valueOf(email.getText());
+                passwordS = String.valueOf(password.getText());
+
+                if (TextUtils.isEmpty(emailS)){
+                    Toast.makeText(SignInActivity.this,"Enter an email Id",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(passwordS)){
+                    Toast.makeText(SignInActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(emailS, passwordS)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignInActivity.this, "Login Successful",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent (getApplicationContext(),MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+
+                                    Toast.makeText(SignInActivity.this, "Authentication failed."+ emailS +passwordS,
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+
+
+            }
+        });
     }
 }
